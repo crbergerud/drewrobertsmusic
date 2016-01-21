3c4d35fd7bb8a9db85bafbfed480811a0c2b7c63
Marty Bergerud <martyb688@gmail.com>
2016-01-20 19:16:21 -0600

    Added more videos and cd link

--- js/theNiche.js
+++ js/theNiche.js
@@ -1,48 +1,26 @@
-console.log("js file loading");
-
-function myFunction() {
-	console.log("myFunction() called");	
-	var x, y
-	console.log("hello there");
-	alert("hi christine");
-	x = 1;
-	y = 2;
-	var z = 3;
-	var sum = x + y + z;
-	var s1 = "hi ";
-	var s2 = "there";
-	var s3 = s1+s2;
-	console.log("the sum of the vars is ="+sum);
+// ie fix
+if (typeof console == "undefined") {
+    console = { log: function () { } };
 }
 
-var isVisible = true; //state variable
-var theElement = document.getElementById("theBox");
+// jquery init
+$(document).ready(function(){
+	console.log("document ready event");
+  // hide the videos div
+  $("#lessVideos").hide();
+  
+   // initialize the click handlers
+  $( "#moreVideosClick" ).click(function() {
+  	console.log("moreVideosClicked")
+  	$("#moreVideos").hide();  	
+  	$("#lessVideos").show();  
+	});
 
-function toggleIt(){
-	console.log("toggleIt() called isVisible = "+isVisible);
-	if ( isVisible == true ){
-		console.log("here1");
-		// hide it
-		theElement.style.visibility = "hidden";
-		isVisible = false;
-		console.log("out isVisible="+isVisible);
-	}
-	else {
-		console.log("here2");
-		//show it
-		theElement.style.visibility = "visible";
-		isVisible = true;
-		console.log("out isVisible="+isVisible);
-	}
+  $( ".lessVideosClick" ).click(function() {
+  	console.log("lessVideosClicked")
+  	$("#moreVideos").show();  	
+  	$("#lessVideos").hide();  
+	});
+	console.log("system initialized");
+})
 
-}
-
-
-function initialize(){
-	console.log("initialize() called e="+theElement);		
-	theElement.style.visibility = "hidden"
-	isVisible = false;
-}
-
-initialize();
-console.log("js file loaded");
